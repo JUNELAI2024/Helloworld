c34a2e9619337ebcfad8e3558803e9d45f9caca3
JUNELAI2024 <159815232+JUNELAI2024@users.noreply.github.com>
2024-03-03 15:00:45 +0800

    change
    change

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,17 @@
-print('Hello World')
+class Solution:
+    def isAangram(self, s:str, t:str) -> bool:
+        return True
+    
+s= "aragram", t "nangram"
+sorted_s ="aaagmnr", sorted_t = "aaagmnr"
+s==t
+
+s="rat" , t="car"
+sorted_s = "art", sorted_t="acr"
+s !=t
+
+class Solution:
+    def isAnagram(self, s: str, t:str)->bool:
+        sorted_s=sorted(s)
+        sorted_t=sorted(t)
+        return sorted_s == sorted_t
